9b4182a026161209bc0de1d5062b8fb31b7ef720
Rus David <davidrus.art@gmail.com>
2017-05-25 12:46:07 +0300

    initial commit - project initialization

---
+++ src/main/java/com/davidrus/shiokosho/ShioKoshoApplication.java
@@ -0,0 +1,12 @@
+package com.davidrus.shiokosho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShioKoshoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShioKoshoApplication.class, args);
+	}
+}
